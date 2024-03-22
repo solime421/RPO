cc2d737437375652dfea2eb74e7041432b5d5bf0
Tamara <adsfsdfs@sfgsdf.com>
2024-03-22 16:36:30 +0300

    Initial commit

---
+++ app/src/main/java/ru/iu3/fclient/MainActivity.java
@@ -0,0 +1,57 @@
+package ru.iu3.fclient;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+import ru.iu3.fclient.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+
+    // Used to load the 'fclient' library on application startup.
+    static {
+        System.loadLibrary("fclient");
+        System.loadLibrary("mbedcrypto");
+    }
+
+    private ActivityMainBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        //getting random numbers
+        int res = initRng();
+        byte[] v = randomBytes(10);
+
+        //poverka biblioteki na sifrovanie
+        byte[] key = new byte[16];
+        byte[] data = "ThisIsMyPassword12345".getBytes();
+
+        byte[] encryptedData = encrypt(key, data);
+        String encryptedString = new String(encryptedData);
+
+        byte[] decryptedData = decrypt(key, encryptedData);
+        String decryptedString = new String(decryptedData);
+
+
+        // Example of a call to a native method
+        TextView tv = binding.sampleText;
+        tv.setText(stringFromJNI());
+    }
+
+    /**
+     * A native method that is implemented by the 'fclient' native library,
+     * which is packaged with this application.
+     */
+    public native String stringFromJNI();
+    public static native int initRng();
+    public static native byte[] randomBytes(int no);
+
+    public static native byte[] encrypt(byte[] key, byte[] data);
+    public static native byte[] decrypt(byte[] key, byte[] data);
+}
